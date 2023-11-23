a06878892e13086ffb1ff7308b90e30df89915c9
Jesus Perez <jesus.perez@culqi.com>
2023-11-23 17:22:52 -0500

    fix: removed inquirer and choices options

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,6 +1,6 @@
 const fs   = require('fs-extra');
 const path = require('path');
-const inquirer = require('inquirer');
+// const inquirer = require('inquirer');
 
 const sourceDir   = path.join(__dirname, '../'); // Directory cypress-craft
 const targetDir   = path.join(__dirname, '../../../'); // Directory YourProject
@@ -23,40 +23,38 @@ const ignoreFiles = [
     'LICENSE',
     'scripts'
 ];
-
-
-inquirer.prompt([
-    {
-        type   : 'list',
-        choices: ['English', 'Spanish'],
-        name   : 'language',
-        message: 'choices the language of your project?'
-    },
-    {
-        type   : 'list',
-        choices: ['E2E', 'API', 'E2E+API'], //None
-        name   : 'typeLogic',
-        message: 'Choice type of project?'
-    },
-    {
-        type   : 'confirm',
-        name   : 'report',
-        message: 'Are you what include report?'
+const filterFunc = (src) => {
+    const basename = path.basename(src);
+    return !ignoreFiles.includes(basename);
+};
+fs.copy(sourceDir, targetDir, {overwrite: true, filter: filterFunc}, function (err) {
+    if (err) {
+        console.error('Error copying files: ', err);
+    } else {
+        console.log('Files copied successfully.');
     }
-    // más preguntas según sea necesario
-]).then(answers => {
-    console.log('Respuesta: ' + JSON.stringify(answers))
-    const filterFunc = (src) => {
-        const basename = path.basename(src);
-        return !ignoreFiles.includes(basename);
-    };
-    fs.copy(sourceDir, targetDir, {overwrite: true, filter: filterFunc}, function (err) {
-        if (err) {
-            console.error('Error copying files: ', err);
-        } else {
-            console.log('Files copied successfully.');
-        }
-    });
-    console.log('Template successfully copied to:', targetDir);
-
 });
+console.log('Template successfully copied to:', targetDir);
+//
+// inquirer.prompt([
+//     {
+//         type   : 'list',
+//         choices: ['English', 'Spanish'],
+//         name   : 'language',
+//         message: 'choices the language of your project?'
+//     },
+//     {
+//         type   : 'list',
+//         choices: ['E2E', 'API', 'E2E+API'], //None
+//         name   : 'typeLogic',
+//         message: 'Choice type of project?'
+//     },
+//     {
+//         type   : 'confirm',
+//         name   : 'report',
+//         message: 'Are you what include report?'
+//     }
+//     // más preguntas según sea necesario
+// ]).then(answers => {
+//     console.log('Respuesta: ' + JSON.stringify(answers))
+// });
