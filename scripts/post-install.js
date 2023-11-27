ba820a070989db90315e7cb6fed959564d338217
Jesus Perez <jesus.perez@culqi.com>
2023-11-27 10:28:21 -0500

    fix: evitar instalación doble o remplazar la carpeta cypress si ya existe

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -1,9 +1,9 @@
 const fs   = require('fs-extra');
 const path = require('path');
-// const inquirer = require('inquirer');
 
-const sourceDir   = path.join(__dirname, '../'); // Directory cypress-craft
-const targetDir   = path.join(__dirname, '../../../'); // Directory YourProject
+const sourceDir   = path.join(__dirname, '../');         // Directory cypress-craft
+const targetDir   = path.join(__dirname, '../../../');   // Directory YourProject
+const cypressDir  = path.join(targetDir, 'cypress/e2e'); // Directory cypress/e2e
 const ignoreFiles = [
     '.idea',
     '.github',
@@ -23,38 +23,20 @@ const ignoreFiles = [
     'LICENSE',
     'scripts'
 ];
+
 const filterFunc = (src) => {
     const basename = path.basename(src);
     return !ignoreFiles.includes(basename);
-};
-fs.copy(sourceDir, targetDir, {overwrite: true, filter: filterFunc}, function (err) {
-    if (err) {
-        console.error('Error copying files: ', err);
-    } else {
-        console.log('Files copied successfully.');
-    }
-});
+}
+if (fs.existsSync(cypressDir)) {
+    console.log('Cypress/e2e directory already exists. Skipping file copy.');
+} else {
+    fs.copy(sourceDir, targetDir, { overwrite: true, filter: filterFunc }, function (err) {
+        if (err) {
+            console.error('Error copying files: ', err);
+        } else {
+            console.log('Files copied successfully.');
+        }
+    });
+}
 console.log('Template successfully copied to:', targetDir);
-//
-// inquirer.prompt([
-//     {
-//         type   : 'list',
-//         choices: ['English', 'Spanish'],
-//         name   : 'language',
-//         message: 'choices the language of your project?'
-//     },
-//     {
-//         type   : 'list',
-//         choices: ['E2E', 'API', 'E2E+API'], //None
-//         name   : 'typeLogic',
-//         message: 'Choice type of project?'
-//     },
-//     {
-//         type   : 'confirm',
-//         name   : 'report',
-//         message: 'Are you what include report?'
-//     }
-//     // más preguntas según sea necesario
-// ]).then(answers => {
-//     console.log('Respuesta: ' + JSON.stringify(answers))
-// });
