408e0dc1b023145b8fed7afbe889066bb10fb492
Jesus Perez <jesus.perez@culqi.com>
2023-11-27 10:35:20 -0500

    fix: refactor in setupNodeEvents.js deleted method deprecated

--- cypress/support/setupNodeEvents.js
+++ cypress/support/setupNodeEvents.js
@@ -1,7 +1,8 @@
-const browserify = require("@badeball/cypress-cucumber-preprocessor/browserify");
+const { preprocessor } = require("@badeball/cypress-cucumber-preprocessor/browserify");
 const {addCucumberPreprocessorPlugin} = require("@badeball/cypress-cucumber-preprocessor");
+
 const setupNodeEvents = async (on, config) => {
-    on("file:preprocessor", browserify.default(config));
+    on("file:preprocessor", preprocessor(config));
     await addCucumberPreprocessorPlugin(on, config);
     return config;
 };
