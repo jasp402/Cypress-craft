e2c47c38c6d36e7c54454eada24c22470287e9b7
Jesus Perez <jesus.perez@culqi.com>
2023-12-21 10:27:53 -0500

    fix: 1.2.2-alpha.1 (2023-12-21)       * completed post-install fish version

--- scripts/post-install.js
+++ scripts/post-install.js
@@ -2,9 +2,12 @@
 const fs   = require('fs-extra');
 const path = require('path');
 const logo = require('./logo');
-const sourceDir   = path.join(__dirname, '../');         // Directory cypress-craft
-const targetDir   = path.join(__dirname, '../../../');   // Directory YourProject
-const cypressDir  = path.join(targetDir, 'cypress/e2e'); // Directory cypress/e2e
+
+const cwd        = process.cwd();               // Directory YourProject
+const sourceDir  = path.join(__dirname, '../'); // Directory cypress-craft
+const targetDir  = path.join(cwd);              // Directory YourProject
+const cypressDir = path.join(cwd, 'cypress');   // Directory cypress/e2e
+
 const ignoreFiles = [
     '.idea',
     '.github',
@@ -23,30 +26,71 @@ const ignoreFiles = [
     'SECURITY.md',
     'LICENSE',
     'scripts',
-    'Writerside'
+    'Writerside',
+    'api_en_steps.txt',
+    'api_es_steps.txt',
+    'e2e_en_steps.txt',
+    'e2e_es_steps.txt',
+    'stepDefinition.core.js'
 ];
 
-function init(language, typeTest){
+
+function init(language, typeTest) {
+    const languageDir   = language === 'English' ? 'tests_es'  : 'tests_en' ;
+
     const filterFunc = (src) => {
         const basename = path.basename(src);
-        return !ignoreFiles.includes(basename);
-    }
+
+        //Ignore files
+        if (ignoreFiles.includes(basename)) {
+            return false;
+        }
+
+        //Exclude language [ES / EN]
+        if (basename.includes(languageDir)) {
+            return false;
+        }
+
+        //Exclude type test [E2E / API]
+        if (typeTest !== 'E2E+API') {
+            const testTypePath = (typeTest === 'API') ? 'e2e' : 'api';
+            if (basename.includes(testTypePath)) {
+                return false;
+            }
+        }
+        return true;
+    };
+
     if (fs.existsSync(cypressDir)) {
-        console.log('Cypress/e2e directory already exists. Skipping file copy.');
+        console.log('Target directory already exists. Skipping file copy.');
     } else {
-        fs.copy(sourceDir, targetDir, { overwrite: true, filter: filterFunc }, function (err) {
+        fs.copy(sourceDir, targetDir, {overwrite: true, filter: filterFunc}, function (err) {
             if (err) {
                 console.error('Error copying files: ', err);
             } else {
                 console.log('Files copied successfully.');
+                if (fs.existsSync(cypressDir)) {
+                    mergeStepDefinitions(language, typeTest, languageDir);
+                }
             }
         });
     }
     console.log('Template successfully copied to:', targetDir);
 }
+
+async function renameFolder(oldPath, newPath) {
+    console.log(`Intentando renombrar de: ${oldPath.split('/').pop()} a: ${newPath.split('/').pop()}`); // Log antes de intentar renombrar
+    try {
+        await fs.rename(oldPath, newPath);
+        console.log(`La carpeta ha sido renombrada de ${oldPath} a ${newPath}`);
+    } catch (error) {
+        console.error('Error al renombrar la carpeta:', error);
+    }
+}
+
 async function main() {
     const inquirer = await import('inquirer');
-    const prompt = inquirer.default.prompt;
+    const prompt   = inquirer.default.prompt;
     prompt([
         {
             type   : 'list',
@@ -58,9 +102,9 @@ async function main() {
         {
             type   : 'list',
             choices: ['E2E', 'API', 'E2E+API'], //None
-            name   : 'typeLogic',
+            name   : 'typeTest',
             message: 'Choice type of project?',
-            default: 'API'
+            default: 'E2E+API'
         },
         {
             type   : 'confirm',
@@ -70,24 +114,22 @@ async function main() {
         }
         // más preguntas según sea necesario
     ]).then(answers => {
-        init(answers.language, answers.type);
-        console.log('Respuesta: ' + JSON.stringify(answers))
-        console.log('Template successfully copied to:', targetDir);
+        console.log('Respuesta: ' + JSON.stringify(answers));
+        init(answers.language, answers.typeTest);
     });
 }
-
 
 async function insertContent(filePath, marker, content) {
     try {
         // Lee el contenido actual del archivo
         let fileContent = await fs.readFile(filePath, 'utf-8');
-        let regexMap = {
+        let regexMap    = {
             API: /(\/\/add steps API\r?\n)/,
             E2E: /(\/\/add steps E2E\r?\n)/
         }
         // Construye un patrón regex para encontrar el marcador
-        const regex = new RegExp(regexMap[marker], 'm');
-        fileContent = fileContent.replace(regex, `${content}\n`);
+        const regex     = new RegExp(regexMap[marker], 'm');
+        fileContent     = fileContent.replace(regex, `${content}\n`);
 
         // Escribe el contenido modificado de vuelta en el archivo
         await fs.writeFile(filePath, fileContent);
@@ -98,42 +140,49 @@ async function insertContent(filePath, marker, content) {
 }
 
 async function mergeStepDefinitions(language, type) {
-    const basePath = '../cypress/common'; // Asegúrate que esta sea la ruta correcta a tus archivos
-    const stepDefinitionPath = path.join(basePath, 'stepDefinition.js');
+    const languageCode       = language === 'Spanish' ? 'es' : 'en';
+    const basePath           = cwd + '/node_modules/cypress-craft/cypress/common';
+    const stepDefinitionPath = path.join(cwd, 'cypress', 'common', 'stepDefinition.js');
+    const testsFolderOld     = path.join(cwd, 'cypress', `tests_${languageCode}`);
+    const testsFolder        = path.join(cwd, 'cypress', `tests`);
 
     // Define los nombres de los archivos basados en el idioma y tipo
     let filesToMerge = [];
-    if (type === 'API' || type === 'API+E2E') {
+    if (type === 'API' || type === 'E2E+API') {
         filesToMerge.push(`api_${language === 'Spanish' ? 'es' : 'en'}_steps.txt`);
     }
-    if (type === 'E2E' || type === 'API+E2E') {
+    if (type === 'E2E' || type === 'E2E+API') {
         filesToMerge.push(`e2e_${language === 'Spanish' ? 'es' : 'en'}_steps.txt`);
     }
-
     // Lee y combina el contenido de los archivos seleccionados
     for (const fileName of filesToMerge) {
-        const filePath = path.join(basePath, fileName);
-        const content = await fs.readFile(filePath, 'utf8');
+        const filePath      = path.join(basePath, fileName);
+        const content       = await fs.readFile(filePath, 'utf8');
         const sectionMarker = fileName.includes('api_') ? 'API' : 'E2E';
         await insertContent(stepDefinitionPath, sectionMarker, content);
     }
 
+    await renameFolder(testsFolderOld, testsFolder);
     console.log('Los contenidos de stepDefinition.js han sido actualizados.');
 }
 
-
-const args = process.argv.slice(2);
+const args   = process.argv.slice(2);
 const params = args.reduce((acc, arg) => {
-    const [key, value] = arg.split(':');
-    acc[key.replace('--', '')] = value;
+    let [key, value] = arg.split(':');
+    key              = key.replace('--', '').toLowerCase();
+    console.log(key);
+    if (!['l', 'language', 't', 'typeTest', 'r', 'report'].includes(key)) throw new Error('Invalid parameter: ' + key);
+    if (key === 'language') key = 'l';
+    if (key === 'typeTest') key = 't';
+    if (key === 'report') key = 'r';
+    acc[key] = value;
     return acc;
 }, {});
 console.log(logo)
-if (!params.language || !params.type) {
+console.log(params);
+if (Object.keys(params).length === 0) {
     main();
 } else {
-    init(params.language, params.type);
+    console.log(params[Object.keys(params)[0]]);
+    init(params[Object.keys(params)[0]], params[Object.keys(params)[1]]);
 }
-
-
-
